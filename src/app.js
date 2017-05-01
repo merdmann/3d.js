5291b2fff125e7ee439b7df738191a9abe427593
MIchael Erdmann <michael.erdmann@snafu.de>
2017-05-01 12:57:50 +0200

    Final touch for https://2017.spaceappschallenge.org/challenges/our-ecological-neighborhood/where-genes-flow/details!

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ import {remote} from "electron";
 import jetpack from "fs-jetpack";
 import env from "./env";
 
+
+function f(x) {
+  
+}
+
 const app = remote.app;
 const appDir = jetpack.cwd(app.getAppPath());
 
@@ -26,23 +31,25 @@ function status(warning) {
 }
 
 var hunter = 2480517;
-var hunted = 2888574;  
+var hunted = 2888574; 
+
+var map = []; 
 
 // 2480517 Bussard
 // 2888574 Mouse
 function loadGBIFData(anchor, gbifId ) {
-
     var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         zoom: 0, 
-	      maxZoom: 18, attribution: '[insert correct attribution here!]' });
+	      maxZoom: 18, attribution: "copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors" });
 
- //  var map = L.map(anchor).setView([52, 13], 13);
-    var map = L.map(anchor, { center: new L.LatLng(51.5, 10), zoom: 0, layers: [osm] });
+    if( !(anchor in map) ) {
+        map[anchor] = L.map(anchor, { center: new L.LatLng(51.5, 10), zoom: 0, layers: [osm] });
+    }
 
     status("Loading data " + anchor + "//" + gbifId );
-    var gbi = L.tileLayer( "http://api.gbif.org/v1/map/density/tile?x={x}&y={y}&z={z}&type=TAXON&key=" + gbifId + "&layer=OBS_2010_2020&layer=LIVING&hue=0.9&palette=yellows_reds", {
-                 attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    var gbi = L.tileLayer( "http://api.gbif.org/v1/map/density/tile?x={x}&y={y}&z={z}&type=TAXON&key=" + gbifId + "&layer=OBS_2010_2020&layer=LIVING&palette=yellows_reds", {
+                 attribution: "&copy; <a href=\"http://www.gbif.org/terms/data-user\">Global Bio Divesity Facility</a> contributors"
+    }).addTo(map[anchor]);
 
     var clouds = L.OWM.clouds({showLegend: false, opacity: 0.5, appId: "457d718f712a87a666b58ae28239b835" });
     var city = L.OWM.current({intervall: 15, lang: 'de'});
@@ -51,10 +58,10 @@ function loadGBIFData(anchor, gbifId ) {
     var baseMaps = { "OSM Standard": osm };
     var overlayMaps = { "Clouds": clouds, "Cities": city, "GBI": gbi };
 
-    var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+    var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map[anchor]);
 
-    L.marker([52, 13]).addTo(map)
-    .bindPopup('Center fo the world')
+    L.marker([52, 13]).addTo(map[anchor])
+    .bindPopup('My location')
     .openPopup();
 
 }
@@ -81,6 +88,12 @@ document.addEventListener("DOMContentLoaded", function() {
       var hunted = getSelection("inp-hunted");
 
       console.log( hunter + "//" + hunted)
+      loadGBIFData( "hunter",  hunter );
+      loadGBIFData( "mouse",  hunted );
+
+      document.getElementById("lbl-hunter").innerHTML = "Species: " + hunter;
+      document.getElementById("lbl-pray").innerHTML = "Species: " +  hunted;
+
     });
 
 function getEventData() {
